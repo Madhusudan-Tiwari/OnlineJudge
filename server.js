e13be6f0fba88e5106d0c3c802fbab17c24d85a2
Madhusudan-Tiwari <algebratrigno@gmail.com>
2026-09-04 18:33:56 +0530

    Initial Express Server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/hello", (req, res) => {res.send("Hello from online judge")});
+
+app.listen(3000,() => {console.log("Server running on port 3000");});
